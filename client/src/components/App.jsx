3471008254156738ee9211450cee87a349adaf55
ChrisRolling <chrisrolling27@gmail.com>
2021-12-05 20:06:13 -0800

    state up

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = { counter: 'hizz' };
+    //this.handleClick = this.handleClick.bind(this);
+  }
+
   render() {
-   return <div> Hello, christopher </div>
+   return <div> Hello {this.state.counter}  </div>
   }
 }
 
