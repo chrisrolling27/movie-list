0dc2cf2cee86fab852a46ba18a9acda760ef03e3
ChrisRolling <chrisrolling27@gmail.com>
2022-01-05 17:47:33 -0800

    paired down app

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,4 @@
 import React from 'react';
-import MovieList from './MovieList.jsx';
-import axios from 'axios';
-
-
-var moviesGiven = [
-  { title: 'Mean Girls' },
-  { title: 'Hackers' },
-  { title: 'The Grey' },
-  { title: 'Sunshine' },
-  { title: 'Ex Machina' },
-];
 
 
 class App extends React.Component {
@@ -18,92 +7,21 @@ class App extends React.Component {
     super(props);
     // Don't call this.setState() here!
     this.state = {
-      movies: [],
-      searchtext: '',
-      query: '',
-      movieadd: '',
+      counter: 42
     };
 
-    this.onChange = this.onChange.bind(this);
-    this.onChange2 = this.onChange2.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.watchButton = this.watchButton.bind(this);
-    this.watchNotButton = this.watchNotButton.bind(this);
+    //this.onChange = this.onChange.bind(this);
   }
 
   componentDidMount() {
-    axios.get('/movies').then((response) => {
-      this.setState({ movies: response.data })
-    })
+    console.log('App mounted');
   }
-
-  onChange(event) {
-    this.setState({ searchtext: event.target.value });
-    event.preventDefault();
-  }
-
-  onChange2(event) {
-    this.setState({ movieadd: event.target.value });
-    event.preventDefault();
-  }
-
-  onClick(event) {
-    this.setState({ query: this.state.searchtext });
-    event.preventDefault();
-  }
-
-  onSubmit(event) {
-    event.preventDefault();
-    if (this.state.movieadd === '') {
-      alert("Please add an actual movie title!");
-    } else {
-      axios.post('/movies', { title: this.state.movieadd, watched: false })
-        .then((response) => {
-          var newMovies = this.state.movies.slice();
-          var newOne = { id: response.data.insertId, title: this.state.movieadd, watched: false };
-          newMovies.push(newOne);
-          this.setState({ movies: newMovies });
-        }
-        )
-    }
-  }
-
-  watchButton() {
-    event.preventDefault();
-    axios.get('/movies/watched')
-    .then((response) => {
-      this.setState({ movies : response.data[1]})
-    })
-  }
-
-  watchNotButton() {
-    event.preventDefault();
-    axios.get('/movies/watched')
-    .then((response) => {
-      this.setState({ movies : response.data[0]})
-    })
-  }
-
 
   render() {
     return (
 
       <div>
-        Total movies: {this.state.movies.length}
-        <form onSubmit={(event) => this.onSubmit(event)}>
-          <input type="text" value={this.state.movieadd} onChange={this.onChange2} placeholder={"Add movie title here"} />
-          <input type="submit" className="greenAddButton" />
-        </form>
-
-        <form>
-          <input type="text" value={this.state.searchtext} onChange={this.onChange} placeholder={"Search..."} />
-          <button onClick={(event) => this.onClick(event)} >Go...</button>
-        </form>
-        <span>&nbsp; </span>
-        <button className='watchButton' onClick={this.watchButton}> Watched </button>
-        <button className='watchNotButton' onClick={this.watchNotButton}> Not Watched </button>
-
-        <MovieList movies={this.state.movies} query={this.state.query} />
+        Hello world
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
-const path = require('path')
-const db = require('../database/connection.js')
+const PORT = 8080;
+const path = require('path');
+
 
 
 app.use(express.json());
 
-db.connect((err) => {
-  if (!err) {
-    console.log('Connected to movie database.');
-  }
-})
+
 
 
 
@@ -20,67 +16,4 @@ app.use(express.static('client/dist'));
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
-})
-
-
-
-app.get('/movies', (req, res) => {
-
-  var querystring = 'SELECT * FROM movies';
-  db.query(querystring, (err, results) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.status(200).send(results);
-    }
-  })
-})
-
-
-app.post('/movies', (req, res) => {
-  var querystring = `INSERT INTO movies (title, watched) VALUES ("${req.body.title}", false)`;
-  console.log('qstring!', querystring);
-  db.query(querystring, (err, results) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      console.log('posted successfully!')
-      res.status(201).send(results);
-    }
-  })
-})
-
-
-app.put('/movies', (req, res) => {
-var querystring = `UPDATE movies SET watched = NOT watched WHERE id = ${req.body.id}`;
-console.log('Put query request:', querystring);
-db.query(querystring, (err, results) => {
-  if (err) {
-    res.status(500).send(err);
-  } else {
-    res.status(204).send(results);
-  }
-})
-})
-
-
-app.get('/movies/watched', (req, res) => {
-  var querystring = 'SELECT * FROM movies';
-  db.query(querystring, (err, results) => {
-    if (err) {
-      console.log(err);
-    } else {
-      var ret = [ [], [] ];
-
-      for (var i = 0; i < results.length; i++) {
-        if (results[i].watched === 0) {
-          ret[0].push(results[i])
-        } else {
-          ret[1].push(results[i])
-        }
-      }
-      //console.log(ret);
-      res.status(200).send(ret);
-    }
-  })
 })
